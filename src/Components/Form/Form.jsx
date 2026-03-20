233c7b7e4a40d688d93fdedeb2041e31b3593018
Vedhaan Technology <vedhaantechnology@gmail.com>
2026-03-20 18:07:46 +0530

    abc

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -74,7 +74,7 @@ const Form = () => {
                     <label>Upload Drawings / BOQ</label>
                     <div className="upload-box">
                         Choose files (PDF / JPG / PNG / Excel)
-                        <input type="file" />
+                        <input name="file" type="file" />
                     </div>
                 </div>
 
